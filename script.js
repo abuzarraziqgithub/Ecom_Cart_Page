097332cfe36cace045ce6af3d8486ae430304c69
abuzarraziqgithub <abuzarraziqo@gmail.com>
2025-05-12 12:07:08 +0500

    Implement product listing and cart functionality

---
+++ script.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("body").style.backgroundColor = "crimson";
+
+  const products = [
+    { id: 1, name: "Product 1", price: 9.99 },
+    { id: 2, name: "Product 2", price: 19.99 },
+    { id: 3, name: "Product 3", price: 59.99 },
+  ];
+
+  const cart = [];
+
+  const productList = document.getElementById("product-list");
+  const cartItems = document.getElementById("cart-items");
+  const emptyCartMessage = document.getElementById("empty-cart");
+  const cartTotalMessage = document.getElementById("cart-total");
+  const totalPriceDisplay = document.getElementById("total-price");
+  const checkOutBtn = document.getElementById("checkout-btn");
+
+  //* Render each product amd use their name, id and price inside the html
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = `
+    <span>${product.name} - $${product.price.toFixed(2)}</span>
+    <button data-id = "${product.id}">Add to cart</button> 
+    `;
+    productList.appendChild(productDiv);
+  });
+
+  productList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const productId = e.target.getAttribute("data-id");
+      const product = products.find((p) => p.id === productId);
+      addToCart(product);
+    }
+  });
+
+  function addToCart(product) {
+    cart.push(product);
+    renderCart();
+  }
+
+  function renderCart() {
+    cartItems.innerText = "";
+    let totalPrice = 0;
+
+    if (cart.length > 0) {
+      emptyCartMessage.classList.add("hidden");
+      cartTotalMessage.classList.remove("hidden");
+      cart.forEach((item, index) => {
+        totalPrice += item.price;
+        const cartItem = document.createElement("div");
+        cartItem.innerHTML = `
+        ${item.name} - $${item.price.toFixed(2)}`;
+        cartItem.appendChild(cartItem);
+        totalPriceDisplay.textContent = `${totalPrice.toFixed(2)}`;
+      });
+    } else {
+      emptyCartMessage.classList.remove("hidden");
+      totalPriceDisplay.textContent = `$0.00`;
+    }
+  }
+
+  checkOutBtn.addEventListener("click", () => {
+    cart.length = 0;
+    alert("Checkout successfully");
+    renderCart();
+  });
+});
